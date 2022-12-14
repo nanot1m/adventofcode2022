2a70e6d333238b1f4aff1708a812f82e04635fcf
Timur Khazamov <teimuraz@miro.com>
2022-12-14 15:22:31 +0100

    Day 14. HTML. Part 2

--- html/src/day14/index.js
+++ html/src/day14/index.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { simulateSand, parseMap } from "../../../js/14.js"
+import { V } from "../../../js/modules/index.js"
 
 const canvas = document.getElementById("canvas")
 if (!(canvas instanceof HTMLCanvasElement)) throw new Error("no canvas")
@@ -7,16 +8,28 @@ if (!(canvas instanceof HTMLCanvasElement)) throw new Error("no canvas")
 const ctx = canvas.getContext("2d")
 if (!ctx) throw new Error("no ctx")
 
-let interval = 0
+let raf = 0
 /**
  * @param {string} input
  * @param {CanvasRenderingContext2D} ctx
  */
-function draw(input, ctx) {
-  clearInterval(interval)
+function draw(input, ctx, part2 = false) {
+  cancelAnimationFrame(raf)
   const map = parseMap(input)
+
+  if (part2) {
+    const h = map.height + 1
+    const minX = Math.min(map.bounds.minX, 500 - h)
+    const maxX = Math.max(map.bounds.maxX, 500 + h)
+
+    const floor = V.segment(V.vec(minX, h), V.vec(maxX, h))
+    for (const pos of floor) {
+      map.set(pos, "~")
+    }
+  }
+
   const { width, height, bounds } = map
-  const scale = Math.min(10, Math.max(4, 100 / width))
+  const scale = Math.min(10, Math.max(2, 200 / width))
 
   ctx.canvas.width = width * scale
   ctx.canvas.height = height * scale
@@ -28,12 +41,12 @@ function draw(input, ctx) {
     o: "orange",
     "#": "gray",
     ".": "darkblue",
+    "~": "brown",
   }
 
   const iter = simulateSand(map)
-  const first = (iter.next().value?.[1] ?? 0) - bounds.minY
 
-  function drawState() {
+  function drawFullState() {
     const map2dStr = map.to2dArray({ valToString: (x) => x ?? "." })
 
     map2dStr.forEach((row, y) => {
@@ -42,20 +55,29 @@ function draw(input, ctx) {
         ctx.fillRect(x * scale, y * scale, scale, scale)
       })
     })
+  }
 
-    let x = 500 - bounds.minX
-    for (let y = 0; y <= first; y++) {
-      ctx.fillStyle = colors["o"]
-      ctx.fillRect(x * scale, y * scale, scale, scale)
+  function drawPoint([x, y], color) {
+    ctx.fillStyle = color
+    ctx.fillRect(
+      (x - bounds.minX) * scale,
+      (y - bounds.minY) * scale,
+      scale,
+      scale,
+    )
+  }
+
+  drawFullState()
+
+  const step = () => {
+    const value = iter.next().value
+    if (value) {
+      drawPoint(value, colors.o)
+      raf = requestAnimationFrame(step)
     }
   }
 
-  drawState()
-
-  interval = setInterval(() => {
-    iter.next()
-    drawState()
-  }, 33)
+  step()
 }
 
 const inputForm = document.getElementById("input-form")
@@ -65,5 +87,5 @@ inputForm.addEventListener("submit", function (e) {
   e.preventDefault()
   const formData = new FormData(this)
   const input = formData.get("input")?.toString() ?? ""
-  draw(input.trim(), ctx)
+  draw(input.trim(), ctx, formData.get("part2") === "on")
 })
